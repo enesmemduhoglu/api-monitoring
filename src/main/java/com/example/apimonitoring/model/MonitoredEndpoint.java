453fe17f4c367a7a77de65c6ca622d17658f226f
enesmemduhoglu <eneshan034@gmail.com>
2026-02-19 17:11:05 +0300

    feat: Add API uptime monitoring service, MonitoredEndpoint model, and initial application configuration.

--- src/main/java/com/example/apimonitoring/model/MonitoredEndpoint.java
+++ src/main/java/com/example/apimonitoring/model/MonitoredEndpoint.java
@@ -1,4 +1,50 @@
 package com.example.apimonitoring.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class MonitoredEndpoint {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String url;
+
+    @Column(nullable = false)
+    private Integer checkIntervalMinutes;
+
+    @Column(nullable = false)
+    private boolean isActive;
+
+    @Column(nullable = false)
+    private String userId;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
 }
--- src/main/java/com/example/apimonitoring/service/UptimeCheckService.java
+++ src/main/java/com/example/apimonitoring/service/UptimeCheckService.java
@@ -1,4 +1,69 @@
 package com.example.apimonitoring.service;
 
+import com.example.apimonitoring.model.MonitoredEndpoint;
+import com.example.apimonitoring.repository.MonitoredEndpointRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class UptimeCheckService {
+    private final MonitoredEndpointRepository endpointRepository;
+
+    private final HttpClient  httpClient =  HttpClient.newBuilder()
+            .connectTimeout(Duration.ofSeconds(10))
+            .build();
+
+    @Scheduled(fixedRate = 60000)
+    public void checkAllEndpoints() {
+        log.info("Checking all endpoints");
+
+        List<MonitoredEndpoint> endpoints = endpointRepository.findByIsActiveTrue();
+
+        for (MonitoredEndpoint endpoint : endpoints) {
+            checkSingleEndpoint(endpoint);
+        }
+    }
+
+    private void checkSingleEndpoint(MonitoredEndpoint endpoint) {
+        long startTime = System.currentTimeMillis();
+        int statusCode = 0;
+        boolean isUp = false;
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(endpoint.getUrl()))
+                    .GET()
+                    .build();
+
+            HttpResponse<Void> response = httpClient.send(request, HttpResponse.BodyHandlers.discarding());
+
+            statusCode = response.statusCode();
+            isUp = (statusCode >= 200 && statusCode < 300);
+
+        } catch (Exception e) {
+            log.error("Hata! {} adresine ulaşılamadı. Sebep: {}", endpoint.getUrl(), e.getMessage());
+        } finally {
+            long responseTime = System.currentTimeMillis() - startTime;
+
+            log.info("Sonuç -> URL: {}, Durum: {}, Kod: {}, Yanıt Süresi: {}ms",
+                    endpoint.getUrl(), (isUp ? "AYAKTA" : "ÇÖKTÜ"), statusCode, responseTime);
+
+            // TODO: İlerleyen adımda bu veriyi Elasticsearch'e göndereceğiz
+            // elasticsearchLogService.savePingResult(endpoint.getId(), endpoint.getUserId(), isUp, statusCode, responseTime);
+        }
+    }
+
+
+
 }
